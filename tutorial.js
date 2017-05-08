6d2879cac084bba69dbc722661615d05f2ae6b23
Unknown <lsannar1@mail.csuchico.edu>
2017-05-08 13:14:06 -0700

    added some styling and fixed margins

--- tutorial.js
+++ tutorial.js
@@ -3,27 +3,27 @@
 var current;
 var graphs=[
   {
-    description:"",
+    description:"This illustrates the point where quantity supplied equals quantity demanded. This is called the Equilibrium",
     image: "http://www.economicshelp.org/wp-content/uploads/2012/11/market-equilibrium.png",
   },
   {
-    description:"",
+    description:"If supply is greater than demand then we have a surplus. Firms will reduce the price until quantity supplied, again, equals quantity demanded.",
     image: "http://www.economicshelp.org/wp-content/uploads/2012/11/excess-supply-move-to-eq.png",
   },
   {
-    description:"",
+    description:"If demand exceeds supply then we have a shortage. Firms will increase prices to reduce demand, until quantity supplied equals quantity demanded.",
     image:"http://www.economicshelp.org/wp-content/uploads/2012/11/excess-demand-move-to-eq-id.png",
   },
     {
-      description:"",
+      description:"When demand shifts out price increases. Quantity demanded also increases. Alternately, if demand decreases price decreases and so does quantity demanded. ",
       image:"http://www.economicshelp.org/wp-content/uploads/2009/09/increase-demand-id.png",
     },
     {
-      description:"",
+      description:"When supply shifts outward,price decreases which also causes quantity demanded to increase. The opposite is true if supply shifts inward(decreases)",
       image:"http://www.economicshelp.org/wp-content/uploads/2009/09/increase-supply-inelastic-demand.png",
     },
     {
-      description:"",
+      description:"For simplicity, and to avoid ambiguity, we shift only one curve at a time and view its affects on the other.Here it is difficult to see which one moved first.",
       image:"http://www.economicshelp.org/wp-content/uploads/2009/09/fall-in-price-id.png",
     },
 ];
@@ -38,6 +38,15 @@ function shuffleGraphs()
   current=randomIndex;
   document.getElementById("image").src = graphs[current].image;
   document.getElementById("caption").innerHTML = graphs[current].description;
+}
+function nextPage(){
+  window.location.href = 'tutorialquiz.html';
+}
+function previousPage(){
+  window.location.href = 'index.html';
+}
+function calculateResults(){
+
 }
 function setCookie(correct, cvalue, exdays) {
     var d = new Date();
