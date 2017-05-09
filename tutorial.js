ead11e074d216a4cb6f18d032986cdf78d75b40d
Unknown <lsannar1@mail.csuchico.edu>
2017-05-09 12:17:32 -0700

    changed index.html

--- tutorial.js
+++ tutorial.js
@@ -1,5 +1,3 @@
-
-
 var current;
 var graphs=[
   {
@@ -87,4 +85,34 @@ function validateAnswer1(){
       document.getElementById("question4").style.color = "red";
    }
 
+ }
+  function setCookie(){
+   var correct =  document.getElementById("group1").value;
+
+ }
+ function calculateResults(){
+   var runningTotal=getcookie("correct");
+   window.location.href='results.html'
+
+ }
+ function setCookie(cname, cvalue, exdays) {
+     var d = new Date();
+     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+     var expires = "expires="+d.toUTCString();
+     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+ }
+
+ function getCookie(cname) {
+     var name = cname + "=";
+     var ca = document.cookie.split(';');
+     for(var i = 0; i < ca.length; i++) {
+         var c = ca[i];
+         while (c.charAt(0) == ' ') {
+             c = c.substring(1);
+         }
+         if (c.indexOf(name) == 0) {
+             return c.substring(name.length, c.length);
+         }
+     }
+     return "";
  }
